0f8e0e6f50a832f35bf9e460ca3e6d85680e5003
Axel Laurent <axosar@gmail.com>
2023-09-13 12:55:32 -0600

    header

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+
+export default function Header(){
+  return(
+    <AppBar>
+      <Toolbar>
+        ARC
+      </Toolbar>
+    </AppBar>
+  )
+}
